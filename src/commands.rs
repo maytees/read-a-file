766d8cb13200e9d844b4ed1186a3fcbce79a8d56
maytees <mateespublicprofile@gmail.com>
2022-04-17 00:19:28 -0400

    commands

---
+++ src/commands.rs
@@ -0,0 +1,5 @@
+//Functions of commands to run
+
+pub fn cmd_hello(args: Vec<String>) {
+    println!("you ran the hello command, congrats");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 use spine;
+use spine::commands;
+
+use std::env;
+use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    parse_commands(args);
+}
+
+fn parse_commands(args: Vec<String>) {
+    match args.get(1) {
+        Some(cmd) => match cmd.as_str() {
+            "hello" => commands::cmd_hello(args),
+            _ => {
+                // is a file
+                let file: String = match args.get(1) {
+                    Some(x) => x,
+                    None() => {}
+                };
+            }
+        },
+        None => {
+            //Display help
+        }
+    }
 }
