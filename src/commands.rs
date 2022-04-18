431bf6eb5d6bfe387898ff065a70ef2ad9d1cbb9
maytees <mateespublicprofile@gmail.com>
2022-04-17 21:35:02 -0400

    print out source & removed command manager

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,5 @@
 //Functions of commands to run
 
-pub fn cmd_hello(args: Vec<String>) {
+pub fn cmd_hello(_args: Vec<String>) {
     println!("you ran the hello command, congrats");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,39 @@ use spine;
 use spine::commands;
 
 use std::env;
-use std::fs::File;
+use std::fs::{OpenOptions};
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    parse_commands(args);
+    let mut source = String::new();
+    
+    parse_commands(args, &mut source);
+
+    println!("{}", &source);
 }
 
-fn parse_commands(args: Vec<String>) {
+fn parse_commands(args: Vec<String>, source: &mut String) -> Result<(), std::io::Error> {
     match args.get(1) {
         Some(cmd) => match cmd.as_str() {
             "hello" => commands::cmd_hello(args),
             _ => {
-                // is a file
-                let file: String = match args.get(1) {
-                    Some(x) => x,
-                    None() => {}
+                let file_path: String = match args.get(1) {
+                    Some(x) => x.clone(),
+                    None => "nothing".to_string(),
                 };
+
+                OpenOptions::new()
+                    .read(true)
+                    .open(file_path)?
+                    .read_to_string(source)?;
             }
         },
         None => {
             //Display help
         }
     }
+
+    Ok(())
 }
